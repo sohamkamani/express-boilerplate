1d731ff9cc9e7691278d73f6c87438b053efca36
sohamkamani <sohamkamani@gmail.com>
2017-02-13 14:25:59 +0530

    Added cors enabled

---
+++ cors-enabled.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('App listening on http://localhost:3000/')
+})
